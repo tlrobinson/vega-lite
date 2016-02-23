1f5d27697f163056ebc7836899094bf0c750a444
Arvind Satyanarayan <arvindsatya1@gmail.com>
2016-02-22 16:41:01 -0800

    Cleanup translate signals.

--- src/compile/selections/translate.ts
+++ src/compile/selections/translate.ts
@@ -47,34 +47,34 @@ export function compileSignals(model: Model, sel: s.Selection, trigger, __, sign
 
 var DIMS = {x: 'width', y: 'height'};
 export function compileData(model: Model, sel: s.Selection, db) {
-  var tx = db.transform, delta = deltaName(sel);
+  var tx = db.transform, anchor = anchorName(sel), delta = deltaName(sel);
   sel.project.forEach(function(p) {
     var field = p.field, channel = p.channel,
         min = 'datum._min_'+field, max = 'datum._max_'+field;
 
     // To prevent aspect ratio drift, capture the current extents
     // and use them in the offset calculation. We need to insert the
-    // delta signal in there to force recomputation.
-    tx.push({
-      type: 'formula',
-      field: '_min_'+field,
-      expr: 'datum.min_'+field+'*(('+delta+'.x/'+delta+'.x)||1)'
-    });
-    tx.push({
-      type: 'formula',
-      field: '_max_'+field,
-      expr: 'datum.max_'+field+'*(('+delta+'.x/'+delta+'.x)||1)'
-    });
-
-    tx.push({
-      type: 'formula',
-      field: 'min_'+field,
-      expr: min + ' + (' + max+'-'+min + ')*'+delta+'.'+channel+'/'+DIMS[channel]
-    });
-    tx.push({
-      type: 'formula',
-      field: 'max_'+field,
-      expr: max + ' + (' + max+'-'+min + ')*'+delta+'.'+channel+'/'+DIMS[channel]
-    });
+    // delta signal in there to force recomputation. Start scale at
+    // zero if there's no anchor.
+    var expr = 'datum.min_'+field+'*(('+delta+'.x/'+delta+'.x)||1)';
+    if (sel.domain) expr = anchor+'.x ? ' + expr + ' : 0';
+    tx.push.apply(tx, [
+      {type: 'formula', field: '_min_'+field, expr: expr},
+      {
+        type: 'formula',
+        field: '_max_'+field,
+        expr: 'datum.max_'+field+'*(('+delta+'.x/'+delta+'.x)||1)'
+      },
+      {
+        type: 'formula',
+        field: 'min_'+field,
+        expr: min + ' + (' + max+'-'+min + ')*'+delta+'.'+channel+'/'+DIMS[channel]
+      },
+      {
+        type: 'formula',
+        field: 'max_'+field,
+        expr: max + ' + (' + max+'-'+min + ')*'+delta+'.'+channel+'/'+DIMS[channel]
+      }
+    ]);
   });
 }
